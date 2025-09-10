4da8e54b3859dd0529cc0a34cf067ad78f8505dd
Eudo08 <e2khovrine92@gmail.com>
2025-09-10 09:39:06 +0200

    clean fichier

---
+++ routes.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from tools_json import load_data, save_data
+
+# Initialisation de l'application Flask
+site = Flask(__name__)
+site.secret_key = "secret_key_for_flashing"
+
+
+FILE_PATH = 'data.json'  # Chemin du fichier de données
+
+
+# Route d'accueil
+@site.route("/")
+def bonjour():
+    # Valeurs par défaut pour les sliders
+    plat = 5
+    # Afficher la page d'accueil avec les valeurs par défaut
+    return render_template("index.html")
+
+
+# Route pour traiter le formulaire de soumission
+@site.route("/submit", methods=["POST", "GET"])
+
+# Exécuter l'application Flask
+# if __name__ == '__main__':
+#     site.run(debug=True)
