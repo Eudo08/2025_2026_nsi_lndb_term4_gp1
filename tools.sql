76e28267f20cb5b1fa7f86e6dcb9b95517668be3
MAMARINEEE <marine.fraboulet@hotmail.com>
2025-09-11 09:52:52 +0200

    avancement tools.sql

--- tools.sql
+++ tools.sql
@@ -1,6 +1,8 @@
+-- FROM interface IMPORT templates, inscription
+
 CREATE DATABASE info_individu
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_general_ci;
 
-INSERT INTO info_individu (nom, prenom, nom_d_utilisateur)
-VALUES ()
+INSERT INTO info_individu (nom, prenom, nom_utilisateur,mot_passe)
+VALUES (Bonnière, Tristan, le caca, abc123)
