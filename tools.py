051b05cfde10ccdc608f7fe2cd1ec7b7de0d5efc
MAMARINEEE <marine.fraboulet@hotmail.com>
2025-09-12 15:12:52 +0200

    tools

--- tools.py
+++ tools.py
@@ -2,14 +2,24 @@ import sqlite3
 con = sqlite3.connect("info_idividu.db")
 cur = con.cursor()
 
-cur.execute("CHARACTER SET utf8mb4")
-cur.execute("COLLATE utf8mb4_general_ci;")
-
-cur.execute("CREATE TABLE information (nom, prenom, pseudo, mot_passe, nb_personne, jour, heure)")
+cur.execute("""
+    CREATE TABLE information (
+        nom TEXT,
+        prenom TEXT,
+        pseudo TEXT,
+        mot_passe TEXT,
+        nb_personne TEXT,
+        jour TEXT,
+        heure TEXT
+    )
+""")
 
 cur.execute("""
     INSERT INTO information VALUES
-        (Eudocie, ABC, ABC, 123, 3, jeudi, 12h),
-        (Arrivée, Tristan, flash, 456, 2, mercredi, 12h)
+        ('Eudocie', 'ABC', 'ABC', '123', '3', 'jeudi', '12h'),
+        ('Arrivée', 'Tristan', 'flash', '456', '2', 'mercredi', '12h')
 """)
+
+
 con.commit()
+con.close()
